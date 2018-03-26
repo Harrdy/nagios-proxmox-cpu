95bd7172681c6dd58b8e355572e7ffb4109ce375
Harrdy <harrdy@xno-sys.de>
2018-03-26 10:05:16 +0200

    Create get_cpu_usage.sh

---
+++ get_cpu_usage.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+sleepDurationSeconds=$1
+
+previousDate=$(date +%s%N | cut -b1-13)
+previousStats=$(cat /proc/stat)
+
+sleep $sleepDurationSeconds
+
+currentDate=$(date +%s%N | cut -b1-13)
+currentStats=$(cat /proc/stat)
+
+OUTPUT=""
+OUTPUT2=""
+
+WARNLIMIT=60
+CRITLIMIT=80
+
+WARN=0
+CRIT=0
+
+cpus=$(echo "$currentStats" | grep -P 'cpu' | awk -F " " '{print $1}')
+
+for cpu in $cpus
+do
+    currentLine=$(echo "$currentStats" | grep "$cpu ")
+    user=$(echo "$currentLine" | awk -F " " '{print $2}')
+    nice=$(echo "$currentLine" | awk -F " " '{print $3}')
+    system=$(echo "$currentLine" | awk -F " " '{print $4}')
+    idle=$(echo "$currentLine" | awk -F " " '{print $5}')
+    iowait=$(echo "$currentLine" | awk -F " " '{print $6}')
+    irq=$(echo "$currentLine" | awk -F " " '{print $7}')
+    softirq=$(echo "$currentLine" | awk -F " " '{print $8}')
+    steal=$(echo "$currentLine" | awk -F " " '{print $9}')
+    guest=$(echo "$currentLine" | awk -F " " '{print $10}')
+    guest_nice=$(echo "$currentLine" | awk -F " " '{print $11}')
+
+    previousLine=$(echo "$previousStats" | grep "$cpu ")
+    prevuser=$(echo "$previousLine" | awk -F " " '{print $2}')
+    prevnice=$(echo "$previousLine" | awk -F " " '{print $3}')
+    prevsystem=$(echo "$previousLine" | awk -F " " '{print $4}')
+    previdle=$(echo "$previousLine" | awk -F " " '{print $5}')
+    previowait=$(echo "$previousLine" | awk -F " " '{print $6}')
+    previrq=$(echo "$previousLine" | awk -F " " '{print $7}')
+    prevsoftirq=$(echo "$previousLine" | awk -F " " '{print $8}')
+    prevsteal=$(echo "$previousLine" | awk -F " " '{print $9}')
+    prevguest=$(echo "$previousLine" | awk -F " " '{print $10}')
+    prevguest_nice=$(echo "$previousLine" | awk -F " " '{print $11}')
+
+    PrevIdle=$((previdle + previowait))
+    Idle=$((idle + iowait))
+
+    PrevNonIdle=$((prevuser + prevnice + prevsystem + previrq + prevsoftirq + prevsteal))
+    NonIdle=$((user + nice + system + irq + softirq + steal))
+
+    PrevTotal=$((PrevIdle + PrevNonIdle))
+    Total=$((Idle + NonIdle))
+
+    totald=$((Total - PrevTotal))
+    idled=$((Idle - PrevIdle))
+
+    CPU_Percentage=$(awk "BEGIN {print ($totald - $idled)/$totald*100}")
+
+    if [[ "$cpu" == "cpu" ]]; then
+        OUTPUT="Total: $CPU_Percentage"
+        OUTPUT2="total=$CPU_Percentage"
+    else
+        if [ ! $(echo "$CPU_Percentage > $WARNLIMIT" | bc -l) -eq 0 ]; then
+                WARN=1
+        fi
+        if [ ! $(echo "$CPU_Percentage > $CRITLIMIT" | bc -l) -eq 0 ]; then
+                CRIT=1
+        fi
+        OUTPUT="$OUTPUT, $cpu $CPU_Percentage%"
+        OUTPUT2="$OUTPUT2 $cpu=$CPU_Percentage"
+    fi
+done
+
+echo "$OUTPUT | $OUTPUT2"
+
+if [ "$CRIT" == "1" ]; then
+        exit 2
+fi
+
+if [ "$WARN" == "1" ]; then
+        exit 1
+fi
+
+exit 0
